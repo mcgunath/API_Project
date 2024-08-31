dcef860ef83fffb8551b95bfca5ae513ff7fd7ec
mcgunath <mcgunath@gmail.com>
2024-08-31 15:51:22 -0400

    a bit of error handling and comments

--- backend/get_wiki.py
+++ backend/get_wiki.py
@@ -23,22 +23,38 @@ headers = {
   'User-Agent': f'{APP_NAME} ({WIKI_EMAIL})'
 }
 
-# build url to call from
+# build url to call
 base_url = 'https://api.wikimedia.org/core/v1/wikipedia/'
 endpoint = '/search/page'
 url = base_url + language_code + endpoint
 parameters = {'q': search_query, 'limit': number_of_results}
 
-# make API call for searching(TODO: ADD ERROR HANDLING)
+# make API call for searching
+
 response = requests.get(url, headers=headers, params=parameters)
+
+# close program if search status is an error
+if response.status_code >= 400:
+    print(f'Error with Search Query ({response.status_code})')
+    raise SystemExit
 
 # convert response to json format and get the first title from the search query
 response = json.loads(response.text)
 title = response['pages'][0]['key']
 
-# make API call for previously retrieved article title and convert to json 
+# make API call for previously retrieved article title
 url = f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{title}'
 response = requests.get(url, headers=headers)
+
+# close program if search status is an error
+if response.status_code >= 400:
+    print(f'Unexpected error with article call ({response.status_code})')
+    raise SystemExit
+
+# convert the response to json
 response = json.loads(response.text)
 
-print(response)
+print(response.keys())
+
+# source appears to be where all the content is, could probably remove left and right square brackets from the text??
+print(response['source'])
