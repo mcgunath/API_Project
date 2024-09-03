66d920962654d9b6242e44f4f69014a2a6ebccba
mcgunath <mcgunath@gmail.com>
2024-09-02 21:35:46 -0400

    page data retrieval now a function

--- backend/get_wiki.py
+++ backend/get_wiki.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
-from utilities.api_tools import search_call
+from utilities.api_tools import search_call, get_page_text
 
 load_dotenv()
 
@@ -17,10 +17,12 @@ headers = {
 }
 
 response = search_call()
-title = response['pages'][0]['key']
+title = response[0]['key']
+
+print(get_page_text(title))
 
 # make API call for previously retrieved article title
-url = f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{title}'
+url = f'https://en.wikipedia.org/w/api.php?action=query&titles={title}|Pear&curtimestamp&prop=links&format=json'
 response = requests.get(url, headers=headers)
 
 # close program if search status is an error
@@ -28,10 +30,17 @@ if response.status_code >= 400:
     print(f'Unexpected error with article call ({response.status_code})')
     raise SystemExit
 
-# convert the response to json
+# convert the response to json, 'parse' encompasses the json
 response = json.loads(response.text)
 
 print(response.keys())
+#print(response['error'])
+timestamp = response['curtimestamp']
+response = response['query']
+pages = response
 
-# source appears to be where all the content is, could probably remove left and right square brackets from the text??
-print(response['source'])
+print(response.keys())
+
+# links that go to other wiki pages can be obtained as so
+print(response['links'])
+print(timestamp)
--- backend/utilities/api_tools.py
+++ backend/utilities/api_tools.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import requests
 import json
 import os
+import re
 
 # load info from .env file
 load_dotenv()
@@ -11,11 +12,12 @@ WIKI_ACCESS_TOKEN = os.getenv('WIKI_ACCESS_TOKEN')
 APP_NAME = os.getenv('APP_NAME')
 WIKI_EMAIL = os.getenv('WIKI_EMAIL')
 
+headers = {
+    'Authorization': f'Bearer {WIKI_ACCESS_TOKEN}',
+    'User-Agent': f'{APP_NAME} ({WIKI_EMAIL})'
+}
+
 def search_call(search_query='pie', language_code='en', number_of_results=1):
-    headers = {
-       'Authorization': f'Bearer {WIKI_ACCESS_TOKEN}',
-       'User-Agent': f'{APP_NAME} ({WIKI_EMAIL})'
-    }
 
     # build url to call
     base_url = 'https://api.wikimedia.org/core/v1/wikipedia/'
@@ -33,4 +35,22 @@ def search_call(search_query='pie', language_code='en', number_of_results=1):
 
     # convert response to json format and get the first title from the search query
     response = json.loads(response.text)
+    return response['pages']
+
+def get_page_text(title):
+    url = f'https://api.wikimedia.org/core/v1/wikipedia/en/page/{title}'
+    response = requests.get(url, headers=headers)
+    # close program if search status is an error
+    if response.status_code >= 400:
+        print(f'Unexpected error with article call ({response.status_code})')
+        return []
+    
+    # convert the response to json, 'parse' encompasses the json
+    response = json.loads(response.text)
+    response = response['source']
+    response = re.sub("{{.*}}", "", response)
+    response = re.sub("==References==.*== Further reading ==", "== Further reading ==", response, flags=re.DOTALL)
+    #response = re.sub("\[\[^(\]).*\]\]", "", response)
+    #response = response.replace("[[", "")
+    #response = response.replace("]]", "")
     return response
