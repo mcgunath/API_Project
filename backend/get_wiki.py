a28e3e25adce747a6b95b70b6e51f8ca7481df59
mcgunath <mcgunath@gmail.com>
2024-09-03 15:31:56 -0400

    added start of function to get all page links

--- backend/get_wiki.py
+++ backend/get_wiki.py
@@ -1,43 +1,4 @@
-import os
-from dotenv import load_dotenv
-import requests
-import json
-from utilities.api_tools import search_call, get_page_text
+from utilities.api_tools import *
 
-load_dotenv()
-
-# TODO: clean up code and make asynchronous
-WIKI_ACCESS_TOKEN = os.getenv('WIKI_ACCESS_TOKEN')
-APP_NAME = os.getenv('APP_NAME')
-WIKI_EMAIL = os.getenv('WIKI_EMAIL')
-
-headers = {
-  'Authorization': f'Bearer {WIKI_ACCESS_TOKEN}',
-  'User-Agent': f'{APP_NAME} ({WIKI_EMAIL})'
-}
-
-response = search_call()
-title = response[0]['key']
-
-url = f'https://en.wikipedia.org/w/api.php?action=query&titles={title}|Pear&curtimestamp&prop=links&format=json'
-response = requests.get(url, headers=headers)
-
-# close program if search status is an error
-if response.status_code >= 400:
-    print(f'Unexpected error with article call ({response.status_code})')
-    raise SystemExit
-
-# convert the response to json, 'parse' encompasses the json
-response = json.loads(response.text)
-
-print(response.keys())
-#print(response['error'])
-timestamp = response['curtimestamp']
-response = response['query']
-pages = response
-
-print(response.keys())
-
-# links that go to other wiki pages can be obtained as so
-print(response['links'])
-print(timestamp)
+links = get_all_pages_links(['Pie'])
+print(links)
--- backend/utilities/api_tools.py
+++ backend/utilities/api_tools.py
@@ -17,6 +17,76 @@ headers = {
     'User-Agent': f'{APP_NAME} ({WIKI_EMAIL})'
 }
 
+def get_all_pages_links(titles):
+    url = f'https://en.wikipedia.org/w/api.php?action=query&titles=Pie|Pear&prop=links&pllimit=max&format=json'
+    response = requests.get(url)
+
+    # close program if status is an error
+    if response.status_code >= 400:
+        print(f'Unexpected error with article call ({response.status_code})')
+        raise SystemExit
+
+    # convert the response to json
+    response = json.loads(response.text)
+
+    # check if more links can be obtained
+    continuation = response['continue'] if 'continue' in response else ''
+    
+    response = response['query']
+    pages = response['pages']
+    links = []
+
+    for pageid in pages:
+        page_links = pages[pageid]['links']
+        for link in page_links:
+            links.append(link['title'])
+
+    if continuation:
+        continuation = continuation['plcontinue']
+        url = f'https://en.wikipedia.org/w/api.php?action=query&titles=Pie|Pear&prop=links&pllimit=max&format=json&plcontinue={continuation}'
+        response = requests.get(url)
+
+    #response = json.loads(response.text)
+    links = list(set(links))
+    links.sort()
+    return links
+
+def get_page_text(title):
+    def cleanup_page_text(text):
+        # Removes references/further reading section
+        # Removes external links
+        # Removes images/gallery
+        # Removes reference links
+        # Removes double curly brace enclosures
+        text = re.sub("==References==.*|==External links==.*|<gallery.*?</gallery>|<ref>.*?</ref>|{{.*?}}|<ref.*?/>", "", text, flags=re.DOTALL)
+        
+        # Removes file lines
+        text = re.sub("^(\[\[)?File.*", "", text, flags=re.MULTILINE)
+        
+        # Removes the square brackets and left side of words that go to other wikis
+        text = re.sub("\[\[([^\|\]\[]*\|([^\|\]\[]*))\]\]", "\g<2>", text, flags=re.DOTALL)
+        # Removes the square brackets of words that go to other wikis
+        text = re.sub("\[\[(.*?)\]\]", "\g<1>", text, flags=re.DOTALL)
+        
+        # Removes additional spaces
+        text = re.sub("\n\n\n+", "\n\n", text)
+        return text
+    
+    url = f'https://en.wikipedia.org/w/index.php?title={title}&action=raw'
+    
+    # make API call for getting page text
+    response = requests.get(url)
+    
+    # close program if status is an error
+    if response.status_code >= 400:
+        print(f'Unexpected error with article call ({response.status_code})')
+        return []
+    
+    # clean the wikitext so that it is readable
+    response = cleanup_page_text(response.text)
+
+    return response
+
 def search_call(search_query='pie', language_code='en', number_of_results=1):
 
     # build url to call
@@ -33,43 +103,8 @@ def search_call(search_query='pie', language_code='en', number_of_results=1):
         print(f'Error with Search Query ({response.status_code})')
         return []
 
-    # convert response to json format and get the first title from the search query
+    # convert response to json format
     response = json.loads(response.text)
+    
+    # return the list of pages found
     return response['pages']
-
-def get_page_text(title):
-    url = f'https://en.wikipedia.org/w/index.php?title={title}&action=raw'
-    
-    # make API call for getting page text
-    response = requests.get(url)
-    
-    # close program if status is an error
-    if response.status_code >= 400:
-        print(f'Unexpected error with article call ({response.status_code})')
-        return []
-    
-    # clean the wikitext so that it is readable
-    response = cleanup_page_text(response.text)
-
-    return response
-
-def cleanup_page_text(text):
-
-    # Removes references section
-    # Removes external links
-    # Removes images/gallery
-    # Removes references/further readings
-    # Removes double curly brace enclosures
-    text = re.sub("==References==.*|==External links==.*|<gallery.*?</gallery>|<ref>.*?</ref>|{{.*?}}|<ref.*?/>", "", text, flags=re.DOTALL)
-    
-    # Removes file lines
-    text = re.sub("^(\[\[)?File.*", "", text, flags=re.MULTILINE)
-    
-    # Removes the square brackets and left side of words that go to other wikis
-    text = re.sub("\[\[([^\|\]\[]*\|([^\|\]\[]*))\]\]", "\g<2>", text, flags=re.DOTALL)
-    # Removes the square brackets of words that go to other wikis
-    text = re.sub("\[\[(.*?)\]\]", "\g<1>", text, flags=re.DOTALL)
-    
-    # Removes additional spaces
-    text = re.sub("\n\n\n+", "\n\n", text)
-    return text
